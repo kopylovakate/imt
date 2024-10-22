41c7a5dffb8d4dbfd0b7eb77ba8bebb062d7cba1
katana <klimenko.katrin888@mail.ru>
2024-10-22 12:07:27 +0300

    reds

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(float rost, int ves) {
-        int imt = (int) (ves/(rost*rost));
+        int imt = (int) (ves / (rost * rost));
 
         return imt;
     }
