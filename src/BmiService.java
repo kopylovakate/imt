4b4d681905b513fb4e212612550ffe6932821c47
katana <klimenko.katrin888@mail.ru>
2024-10-24 16:38:37 +0300

    eng

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(float rost, int ves) {
-        int imt = (int) (ves / (rost * rost));
+    public int calculate(float height, int weight) {
+        int imt = (int) (weight / (height * height));
 
         return imt;
     }
